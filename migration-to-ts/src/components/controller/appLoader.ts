7ad57e907f4e14408f2f2c97755ca5f39bd74e33
Anastasia Mataeva <ana.web@mail.ru>
2023-06-11 20:33:42 +0300

    fix: change AppLoader url

--- migration-to-ts/src/components/controller/appLoader.ts
+++ migration-to-ts/src/components/controller/appLoader.ts
@@ -2,12 +2,12 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        // super('https://newsapi.org/v2/', {
-        //     apiKey: '13ecd8a3b11342a1bca792e17fc3e235', // получите свой ключ https://newsapi.org/
-        // });
-        super('http://127.0.0.1:8075/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+        super('https://newsapi.org/v2/', {
+            apiKey: '13ecd8a3b11342a1bca792e17fc3e235', // получите свой ключ https://newsapi.org/
         });
+        // super('http://127.0.0.1:8075/', {
+        //     apiKey: '', // получите свой ключ https://newsapi.org/
+        // });
     }
 }
 
