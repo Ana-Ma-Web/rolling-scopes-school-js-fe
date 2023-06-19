70631e88eb2a68d9fb86cbb1d33f2004edf6f56c
Anastasia Mataeva <ana.web@mail.ru>
2023-06-19 15:35:22 +0300

    refactor: remove unnecessary types

--- migration-to-ts/src/components/view/news/news.ts
+++ migration-to-ts/src/components/view/news/news.ts
@@ -11,13 +11,13 @@ class News {
     };
 
     private drawNewsClone(newsClone: DocumentFragment, item: Article): void {
-        const newsMetaPhoto = this.getElement<HTMLElement>(newsClone, '.news__meta-photo');
-        const newsMetaAuthor = this.getElement<HTMLElement>(newsClone, '.news__meta-author');
-        const newsMetaDate = this.getElement<HTMLElement>(newsClone, '.news__meta-date');
-        const newsDescTitle = this.getElement<HTMLElement>(newsClone, '.news__description-title');
-        const newsDescSource = this.getElement<HTMLElement>(newsClone, '.news__description-source');
-        const newsDescContent = this.getElement<HTMLElement>(newsClone, '.news__description-content');
-        const newsReadMoreLink = this.getElement<HTMLElement>(newsClone, '.news__read-more a');
+        const newsMetaPhoto = this.getElement(newsClone, '.news__meta-photo');
+        const newsMetaAuthor = this.getElement(newsClone, '.news__meta-author');
+        const newsMetaDate = this.getElement(newsClone, '.news__meta-date');
+        const newsDescTitle = this.getElement(newsClone, '.news__description-title');
+        const newsDescSource = this.getElement(newsClone, '.news__description-source');
+        const newsDescContent = this.getElement(newsClone, '.news__description-content');
+        const newsReadMoreLink = this.getElement(newsClone, '.news__read-more a');
 
         newsMetaPhoto.style.backgroundImage = `url(${item.urlToImage || 'img/news_placeholder.jpg'})`;
         newsMetaAuthor.textContent = item.author || item.source.name;
